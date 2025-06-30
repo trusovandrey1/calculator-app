551b61aa1a73fb2a3e2c72f2cd2c0c2d71661847
trusovandrey1 <48606874+trusovandrey1@users.noreply.github.com>
2025-06-30 22:52:37 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.end('<html><body><h1>Hello, World!</h1></body></html>');
 });
 
-const port = 3000;
+const port = 3001;
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
